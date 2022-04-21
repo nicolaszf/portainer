3ea7155ecb036a5607fcb9ff503acbaca35805b9
Chaim Lev-Ari <chiptus@gmail.com>
2022-04-21 11:31:06 +0300

    refactor(app): make utils for react-query

--- app/portainer/tags/queries.ts
+++ app/portainer/tags/queries.ts
@@ -1,6 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 
-import { error as notifyError } from '@/portainer/services/notifications';
+import {
+  mutationOptions,
+  withError,
+  withInvalidate,
+} from '@/react-tools/react-query';
 
 import { createTag, getTags } from './tags.service';
 import { Tag } from './types';
@@ -9,9 +13,7 @@ export function useTags<T = Tag>(select?: (tags: Tag[]) => T[]) {
   const { data, isLoading } = useQuery(['tags'], () => getTags(), {
     staleTime: 50,
     select,
-    onError(error) {
-      notifyError('Failed loading tags', error as Error);
-    },
+    ...withError('Failed to retrieve tags'),
   });
 
   return { tags: data, isLoading };
@@ -20,15 +22,11 @@ export function useTags<T = Tag>(select?: (tags: Tag[]) => T[]) {
 export function useCreateTagMutation() {
   const queryClient = useQueryClient();
 
-  return useMutation(createTag, {
-    meta: {
-      error: {
-        message: 'Unable to create tag',
-        title: 'Failure',
-      },
-    },
-    onSuccess() {
-      queryClient.invalidateQueries(['tags']);
-    },
-  });
+  return useMutation(
+    createTag,
+    mutationOptions(
+      withError('Unable to create tag'),
+      withInvalidate(queryClient, [['tags']])
+    )
+  );
 }
